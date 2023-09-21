d19e36f411dc4a2efa51d0372a767164d127b1a6
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-09-21 14:10:46 +0300

    ex 4 was done

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-try:
-    print()
-except Exception as e:
-    print(e)
+number1 = float(input ("Введіть лерше число: "))
+number2 = float (input ("Введіть друге числа: "))
+
+if number1 == number2:
+    print ("Числа рівні.")
+else:
+    if number1 < number2:
+        print( "Числа у порядку зростання:", number1, number2)
+    else:
+        print( "Числа у порядку зростання:", number2, number1)
