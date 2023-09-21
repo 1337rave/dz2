97433b6d92e032e545c574ae1156a70612b85231
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-09-21 13:52:27 +0300

    ex 1 was done

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-try:
-    print()
-except Exception as e:
-    print(e)
+day_number = int(input ("1: "))
+
+if day_number == 1:
+    day_name = "понеділок"
+elif day_number == 2:
+    day_name = "вівторок"
+elif day_number == 3:
+    day_name = "середа"
+elif day_number == 4:
+    day_name = "четверг"
+elif day_number == 5:
+    day_name = "п'ятниця"
+elif day_number == 6:
+    day_name = "субота"
+elif day_number == 7:
+    day_name = "неділя"
+
+print("Назва дня тижня: ", day_name)
