76ae1e0a0759bd396b146ff01152a716b02b2442
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-09-21 14:04:16 +0300

    ex 3 was done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-try:
-    print()
-except Exception as e:
-    print(e)
+# отримання числа від кавистнеата
+number = float(input( "Введіть число: "))
+
+if number > 0:
+    print ("Number is positive")
+elif number < 0:
+    print("Number is negative")
+else:
+    print ("Number is equal to zero")
